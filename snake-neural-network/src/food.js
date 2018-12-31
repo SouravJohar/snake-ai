c9df5fb12b9e9945d9af689aa736bf63baa0252c
Vignesh Kumar <xxvignesh@gmail.com>
2018-12-31 18:23:20 +0530

    removed nested

--- snake-neural-network/src/food.js
+++
@@ -1,8 +0,0 @@
-class Food {
-
-  constructor (game) {
-    const availablePositions = game.getAvailablePositions()
-    this.position = availablePositions[Math.floor(Math.random() * availablePositions.length)]
-  }
-
-}
